613e2afb2ce3928f8d4119d11449877321d4852a
María del Rocío Romo García <bloomblaze7896@gmail.com>
2025-01-09 16:17:01 +0100

    Cansada de Git...

---
+++ Lab 1.sql
@@ -0,0 +1,16 @@
+SELECT * FROM film;
+
+SELECT name AS language FROM language;
+
+SELECT first_name AS staff FROM staff;
+
+SELECT DISTINCT release_year AS film FROM film;
+
+SELECT COUNT(*) AS store FROM store;
+
+SELECT COUNT(*) AS staff FROM staff;
+
+SELECT COUNT(*) AS rental_id FROM rental; 
+
+SELECT COUNT(DISTINCT last_name) AS distinct_last_names FROM actor;
+
