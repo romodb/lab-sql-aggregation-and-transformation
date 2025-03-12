070d9b0061767f17da3b862969554047a190c9f9
María del Rocío Romo García <bloomblaze7896@gmail.com>
2025-03-12 02:31:22 +0100

    Ping

--- Lab 1.sql
+++ Lab 1.sql
@@ -1,22 +1,23 @@
+
+
 -- Retp 1:
 -- Ejercicio 1: Consigue y almacena los valores extremos.
 
 SET @min_duracion = (SELECT MIN(length) FROM film);
-SET @max_duraci = (SELECT MAX(length) FROM film);
+SET @max_duracion = (SELECT MAX(length) FROM film);
+SELECT @min_duracion, @max_duracion;
 
 -- Expresa la duracion promedio de la peli en horas y minutos.
 
-SELECT FLOOR(@min_duraci);
-SELECT FLOOR(@max_duracion);
-SELECT ROUND(LENGTH, 2) FROM film;
+SELECT FLOOR(AVG(length)/60) AS hours, ROUND(AVG(length) % 60) 
+AS minutos FROM film;
 
 /* Ejercicio 2:
 Numero de dias que la empresa estuvo en funcionamiento:
 */
 
-SELECT FLOOR(@min_date = (SELECT MIN(rental_date) FROM rental);
-SELECT FLOOR(@max_date = (SELECT MAX(return_date) FROM rental);
-SELECT DATEDIFF(@min_date, @max_date);
+SELECT DATEDIFF(MAX(rental_date), MIN(rental_date)) AS active_days
+FROM rental;
 
  
 -- Usando la informacion del alquiler añade dos columnas para el mes y el dia de la semana. Saca los primeros 20 resultados.
